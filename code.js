2028fb5c473280466b623e9da133d612cb630ff3
Drew Pavlik <drewpavlik@Drews-MBP-2.attlocal.net>
2020-08-03 23:16:38 -0400

    put in 1 black key

--- code.js
+++ code.js
@@ -2,6 +2,8 @@ function alertKey(octave, num) {
     var string = 0;
     var fret = 0;
     var pianoSum = (octave * 12) + num - 4;
+
+    // accounts for the highest string
     if ((octave == 2 && num > 8) || octave > 2) {
 	string = 5;
 	fret = ((octave - 2) * 12) + num - 4;
@@ -11,4 +13,15 @@ function alertKey(octave, num) {
     }
     alert("octave: " + octave + "   num: " + num + "\n"
     + "corresponding string: " + string + "    corresponding fret: " + fret);
+}
+
+function getWX1(numb) {
+    var x1 = 94 + (numb * 45);
+    //return num.toString((94 + (numb * 45)));
+    return x1;
+}
+
+function getWX2(numb) {
+    var x2 = 139 + (numb * 45);
+    //return num.toString((139 + (numb * 45)));
 }

82a2524f7f256d3cea058676e7c41b6616733a18
Drew Pavlik <drewpavlik@Drews-MBP-2.attlocal.net>
2020-08-03 21:46:05 -0400

    Added algorithm for piano to guitar

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
-function alertKey(num, note) {
-    alert("you pressed key #" + num + " and its note is " + note);
+function alertKey(octave, num) {
+    var string = 0;
+    var fret = 0;
+    var pianoSum = (octave * 12) + num - 4;
+    if ((octave == 2 && num > 8) || octave > 2) {
+	string = 5;
+	fret = ((octave - 2) * 12) + num - 4;
+    } else {
+	string = Math.floor(pianoSum / 5);
+	fret = pianoSum % 5;
+    }
+    alert("octave: " + octave + "   num: " + num + "\n"
+    + "corresponding string: " + string + "    corresponding fret: " + fret);
 }
